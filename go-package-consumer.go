2c4f3a1b89da50ea02c1c017c95685543d84ccd5
noprom <tyee.noprom@qq.com>
2017-06-08 13:27:30 +0800

    add npc

---
+++ go-package-consumer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/olekukonko/tablewriter"
+	"os"
+)
+
+func main() {
+	data := [][]string{
+
+		[]string{"Alfred", "15", "10/20", "(10.32, 56.21, 30.25)"},
+		[]string{"Beelzebub", "30", "30/50", "(1,1,1)"},
+		[]string{"Hortense", "21", "80/80", "(1,1,1)"},
+		[]string{"Pokey", "8", "30/40", "(1,1,1)"},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"NPC", "Speed", "Power", "Location"})
+	table.AppendBulk(data)
+	table.Render()
+}
